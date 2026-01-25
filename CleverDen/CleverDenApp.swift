998bfa184b43e06ed225e4ecbd9e6a1ae6af77b3
maycry <byorickq@gmail.com>
2026-01-24 20:42:41 -0800

    Initial Commit

---
+++ CleverDen/CleverDenApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleverDenApp.swift
+//  CleverDen
+//
+//  Created by Iurii Tanskyi on 1/24/26.
+//
+
+import SwiftUI
+
+@main
+struct CleverDenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
